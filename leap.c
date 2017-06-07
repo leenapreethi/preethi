e107fe53195ce44bebc21d39d0ba53601be1983a
leenapreethi <cpreethiindhu1997@gmail.com>
2017-06-07 11:42:05 +0530

    Add files via upload

---
+++ leap.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int year;
+printf("enter the year:");
+scanf("%d",&year);
+if(year%4==0)
+{
+printf(" leap year");
+}
+else
+{
+printf(" not an leap year");
+}
+return 0;
+getch();
+}
