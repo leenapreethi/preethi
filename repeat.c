69535a152b3d0b349913ec60f4f6e3265fb51d0a
leenapreethi <cpreethiindhu1997@gmail.com>
2017-06-28 10:26:33 +0530

    Create repeat.c

---
+++ repeat.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+   long int a[20],i,j,p,temp,c;
+   long int sum=0;
+   clrscr();
+   scanf("%ld",&p);
+for(i=0;i<p;i++)
+{
+  scanf("%d",&a[i]);
+}
+for(i=0;i<p;i++)
+{
+ for(j=i+1;j<p;j++)
+ {
+  if(a[j]>a[i])
+ {
+  temp=a[i];
+  a[i]=a[j];
+  a[j]=temp;
+ }
+ }
+}
+for(i=0;i<p-1;i++)
+{
+  temp=a[i+1];
+  c=0;
+  while(temp!=0)
+  {
+    temp=temp/10;
+    c++;
+  }
+  sum=(sum*pow(10,c))+a[i+1];
+}
+printf("\n%ld",sum);
+getch();
+}
