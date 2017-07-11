903c1ce3814cf4b1e353a9cd9469ed6803f73403
leenapreethi <cpreethiindhu1997@gmail.com>
2017-07-11 10:13:39 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int m,n,p;
+printf("enter m number");
+scanf("%d",&m);
+printf("enter n number");
+scanf("%d",&n);
+p=pow(m,n);
+printf("%d power of a number",p);
+getch();
+}
